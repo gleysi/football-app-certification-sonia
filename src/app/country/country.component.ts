363ea4be217edbea02d00ec3e3466f5ca25fe448
Sonia Miranda <sonia.mireya.miranda@gmail.com>
2023-10-22 22:49:41 -0600

    standings page

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FootballService } from '../services/football.service';
+import { Observable, Subscription } from 'rxjs';
+import { Standings } from '../interfaces/standings.interface';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.css']
 })
-export class CountryComponent {
+export class CountryComponent implements OnInit, OnDestroy {
+  //public standingsResponse: Observable<Standings>;
+  public subscription: Subscription;
+  public standings: any;
+  //public league: any;
 
+  constructor(private footballService: FootballService) {
+
+    this.subscription = this.footballService.getStandings().subscribe((standings) => {
+      //const standings = response<Standings>;
+      if (standings && standings.response && standings.response[0] && standings.response[0].league && standings.response[0].league.standings && standings.response[0].league.standings[0]) {
+        this.standings = standings.response[0].league.standings[0];
+      } else {
+        this.standings = [];
+      }
+      console.log(this.standings);
+    });
+  }
+
+  ngOnInit(): void {
+    //this.getStandings();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  getStandings() {
+    // this.subscription = this.footballService.getStandings().subscribe((data) => {
+    //   console.log(data);
+    // });
+  }
 }
--- src/app/services/football.service.ts
+++ src/app/services/football.service.ts
@@ -2,24 +2,41 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Countries } from '../interfaces/countries.interface';
 import { Observable } from 'rxjs';
+import { Standings } from '../interfaces/standings.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FootballService {
-  baseUrl = 'https://v3.football.api-sports.io/';
-  apiKey = 'a60d8370e32cee7fe3e96b6fdc94cfd2';
+  private baseUrl = 'https://v3.football.api-sports.io/';
+  private apiKey = '920753df6ed093838e527fe46e6be439';
+
+  private headers = {
+    'x-rapidapi-host': 'v3.football.api-sports.io',
+    'x-rapidapi-key': '920753df6ed093838e527fe46e6be439'
+  }
+
+
   private countries_json = './assets/countries.json';
 
   constructor(private _http: HttpClient) {
     
   }
 
+  // TODO: Check countries json url
   getCountries(): Observable<Countries[]> {
     const headers = new HttpHeaders()
     .set('content-type', 'application/json')
     .set('Access-Control-Allow-Origin', '*');
     return this._http.get<Countries[]>(this.countries_json,  { 'headers': headers });
+  }
+
+  getStandings(): Observable<any> {
+    const params = {
+      league: '39',
+      season: '2023'
+    };
+    return this._http.get<Standings>(this.baseUrl + '/standings', { params, headers: this.headers })
   }
 
 }
