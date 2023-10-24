82198daac5a21cc203bc979c8800255bd1d675a6
Sonia Miranda <sonia.mireya.miranda@gmail.com>
2023-10-24 00:52:27 -0600

    cache game results

--- src/app/services/football.service.ts
+++ src/app/services/football.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Countries } from '../interfaces/countries.interface';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 import { StandingsModel } from '../interfaces/standings.interface';
 import { FixturesResponse, FixturesModel } from '../interfaces/fixture.interface';
 
@@ -13,6 +13,7 @@ export class FootballService {
   private baseUrl = 'https://v3.football.api-sports.io/';
   private apiKey = '920753df6ed093838e527fe46e6be439';
   private currentSeason = new Date().getFullYear();
+  private currentDate = new Date().getTime();
 
   private headers = {
     'x-rapidapi-host': 'v3.football.api-sports.io',
@@ -35,6 +36,12 @@ export class FootballService {
   }
 
   getResults(teamId: number, last:number): Observable<FixturesResponse[]> {
+    const cacheKey = `${this.baseUrl}/fixtures?team=${teamId}&last=${last}&season=${this.currentSeason}`;
+    const isDataCatched = this.getCache(cacheKey);
+    if (isDataCatched) {
+      return of(isDataCatched as FixturesResponse[]);
+    }
+
     const params = {
       team: teamId,
       last,
@@ -42,10 +49,30 @@ export class FootballService {
     }
     return this._http.get<FixturesModel>(this.baseUrl + 'fixtures', { params, headers: this.headers }).pipe(
       map((data: FixturesModel) => {
+        this.setCache(cacheKey, data.response as FixturesResponse[], this.currentDate.toString());
         return data && data.response.length ? data.response as FixturesResponse[]: [];
       })
     );
   }
 
+  setCache(key: string, data: FixturesResponse[], storedDate: string) {
+    localStorage.setItem(key, JSON.stringify({ data, storedDate }));
+  }
+
+  getCache(key: string): any {
+    const storedData = localStorage.getItem(key);
+    if (storedData) {
+      const { data, storedDate } = JSON.parse(storedData);
+      const diff = Math.abs(this.currentDate - parseInt(storedDate)) / 3600000;
+      const hours = Math.ceil(diff / (1000 * 60 * 60));
+      if (hours < 8) {
+        return data;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
 
 }
