a2779b9bb780a7922c21a09ffdb077a6b7778837
Sonia Miranda <sonia.mireya.miranda@gmail.com>
2023-10-24 01:31:50 -0600

    Added cache to standings

--- src/app/services/football.service.ts
+++ src/app/services/football.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Countries } from '../interfaces/countries.interface';
 import { Observable, map, of } from 'rxjs';
 import { StandingsModel, CountryStandings } from '../interfaces/standings.interface';
@@ -11,7 +11,6 @@ import { FixturesResponse, FixturesModel } from '../interfaces/fixture.interface
 export class FootballService {
   public countries_json_url: string = 'assets/countries.json';
   private baseUrl = 'https://v3.football.api-sports.io/';
-  private apiKey = '920753df6ed093838e527fe46e6be439';
   private currentSeason = new Date().getFullYear();
   private currentDate = new Date().getTime();
 
@@ -28,12 +27,18 @@ export class FootballService {
   }
 
   getStandings(league: number): Observable<CountryStandings> {
+    const cacheKey = `${this.baseUrl}/standings?league=${league}&season=${this.currentSeason}`;
+    const isDataCatched = this.getCache(cacheKey);
+    if (isDataCatched) {
+      return of(isDataCatched as CountryStandings);
+    }
     const params = {
       league: league,
       season: this.currentSeason
     };
     return this._http.get<StandingsModel>(this.baseUrl + '/standings', { params, headers: this.headers }).pipe(
       map((data: StandingsModel) => {
+        this.setCache(cacheKey, data.response[0].league.standings[0] as CountryStandings, this.currentDate.toString());
         return data.response[0].league.standings[0] as CountryStandings;
       })
     );
@@ -59,17 +64,17 @@ export class FootballService {
     );
   }
 
-  setCache(key: string, data: FixturesResponse[], storedDate: string) {
+  setCache(key: string, data: FixturesResponse[] | CountryStandings, storedDate: string): void {
     localStorage.setItem(key, JSON.stringify({ data, storedDate }));
   }
 
-  getCache(key: string): any {
+  getCache(key: string): FixturesResponse[] | CountryStandings | null {
     const storedData = localStorage.getItem(key);
     if (storedData) {
       const { data, storedDate } = JSON.parse(storedData);
       const diff = Math.abs(this.currentDate - parseInt(storedDate)) / 3600000;
       const hours = Math.ceil(diff / (1000 * 60 * 60));
-      if (hours < 8) {
+      if (hours < 12) {
         return data;
       } else {
         return null;
